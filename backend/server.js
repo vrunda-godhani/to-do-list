8b68a873f43bf592a8d202240d00bb8509bd6be3
vrunda godhani <vrundagodhani04@gmail.com>
2025-05-29 13:21:21 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
