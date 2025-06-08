be4055b27cc46741bcfc5ca4c6f5f5ee5c228517
vrunda godhani <vrundagodhani04@gmail.com>
2025-06-08 18:57:46 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ if (!JWT_SECRET) {
 
 app.use(
   cors({
-    origin: ["https://to-do-list-hvllb6ziv-vrunda-s-projects.vercel.app"],
+    origin: ["https://to-do-list-eta-liart-42.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
