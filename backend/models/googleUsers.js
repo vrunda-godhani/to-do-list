ee18c238c483b87ecd53422e65d37531fee1bc6c
vrunda godhani <vrundagodhani04@gmail.com>
2025-06-28 22:57:42 +0530

    commit

--- backend/models/googleUsers.js
+++ backend/models/googleUsers.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const googleUserSchema = new mongoose.Schema({
-  google_id: { type: String, required: true, unique: true },
-  name: String,
-  email: { type: String, required: true },
-  profile_pic: String
-});
+// const googleUserSchema = new mongoose.Schema({
+//   google_id: { type: String, required: true, unique: true },
+//   name: String,
+//   email: { type: String, required: true },
+//   profile_pic: String
+// });
 
-module.exports = mongoose.model("GoogleUser", googleUserSchema);
+// module.exports = mongoose.model("GoogleUser", googleUserSchema);
--- backend/models/tasks.js
+++ backend/models/tasks.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  user_id: {
-    type: String, // 🔄 changed from ObjectId
-    required: true
-  },
-  task_text: String,
-  task_date: Date,
-  priority: String,
-  is_done: {
-    type: Boolean,
-    default: false
-  }
-});
+// const TaskSchema = new mongoose.Schema({
+//   user_id: {
+//     type: String, // 🔄 changed from ObjectId
+//     required: true
+//   },
+//   task_text: String,
+//   task_date: Date,
+//   priority: String,
+//   is_done: {
+//     type: Boolean,
+//     default: false
+//   }
+// });
 
-module.exports = mongoose.model("Task", TaskSchema);
+// module.exports = mongoose.model("Task", TaskSchema);
