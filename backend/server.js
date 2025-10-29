aa531bf2d40457f11f24e29184ebb709babc5d3f
vrunda godhani <vrundagodhani04@gmail.com>
2025-10-29 16:42:20 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -48,16 +48,27 @@ app.use(express.json());
 //     queueLimit: 0,
 // });
 
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     PORT: process.env.PORT || 3306,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
+
+
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    PORT: process.env.PORT || 3306,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  socketPath: process.env.DB_HOST.includes("/cloudsql/") ? process.env.DB_HOST : undefined,
+  port: process.env.DB_PORT || 3306,
 });
+
 
 const multer = require("multer");
 const path = require("path");
